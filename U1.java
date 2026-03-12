99f60b15807e169f305c4d8659776c070ffc9429
Thadius <entropy050@gmail.com>
2026-03-12 15:48:32 +0530

    Checked for Pallindrome using Object-oriented approach of Programming

--- U1.java
+++ U1.java
@@ -1,28 +1,42 @@
 import java.util.*;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
-class U1{
-public static int version = 1;
+public class U1{
+    public static final int VERSION = 1;
+    static class PallindromeChecker{
+        private String original;
+        private char[] input;
+        public PallindromeChecker(String in){
+            this.original = in;
+            this.input = preProcess(in).toCharArray();
+        }
+        public String preProcess(String x){
+            String y = x.replaceAll("\\s", "");
+            y = x.toLowerCase();
+            return y;
+        }
+        public boolean check(){
+            int r= input.length-1;
+            for(int i=0; i<r/2; i++,r--){
+            if(input[i]!=input[r]){return false;}
+            }
+            return true;
+        }
+        public void result(){
+            System.out.printf("Is \"%s (preprocessed: %s)\" a pallindrome? %s", original, new String(input), check());
+        }
+    }
     public static void main(String[] args){
         try {
             System.out.println("Welcome to Palindrome Vhecker Management System");
-            System.out.printf("Version: %d%n", version);
+            System.out.printf("Version: %d%n", VERSION);
         } catch (java.lang.RuntimeException e) {
             throw new RuntimeException(e);
         }
         System.out.println("System initialized successfully");
-        String str = "Madam";
+        String s = "Madam";
 
-        str = str.replaceAll("\s", "");
-        str = str.toLowerCase();
-
-        int l=0, r=str.length()-1;
-        while(l<=r){
-            if(str.charAt(l)!=str.charAt(r)){System.out.println("Is not a pallindrome"); return;}
-        }
-        System.out.println("Is a pallindrome");
-
-
+        PallindromeChecker service = new PallindromeChecker(s);
+        service.result();
     }
+    
 }
     
