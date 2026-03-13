22def9f9bb1b30a7cd85df56cc702ad089623e94
Thadius <entropy050@gmail.com>
2026-03-13 13:28:44 +0530

    Strategy Pattern for Palindrome Algorithms--Choosing a palindrome algorithm dynamically.

--- U1.java
+++ U1.java
@@ -1,27 +1,64 @@
 import java.util.*;
 public class U1{
     public static final int VERSION = 1;
-    static class PallindromeChecker{
-        private String original;
-        private char[] input;
-        public PallindromeChecker(String in){
-            this.original = in;
-            this.input = preProcess(in).toCharArray();
-        }
-        public String preProcess(String x){
+    static interface PallindromeChecker{
+        public static String preProcess(String x){
             String y = x.replaceAll("\\s", "");
             y = x.toLowerCase();
             return y;
         }
-        public boolean check(){
-            int r= input.length-1;
-            for(int i=0; i<r/2; i++,r--){
-            if(input[i]!=input[r]){return false;}
+        public default boolean check(char[] input){
+            for(int l=0, r= input.length; l<r; l++,r--){
+            if(input[l]!=input[r]){return false;}
             }
             return true;
         }
-        public void result(){
-            System.out.printf("Is \"%s (preprocessed: %s)\" a pallindrome? %s", original, new String(input), check());
+        public default void result(boolean res, String original, char[] input){
+            System.out.printf("Is \"%s (preprocessed: %s)\" a pallindrome? %s", original, new String(input), res);
+        }
+        public void printResult();
+    }
+
+    static class StackCheck implements PallindromeChecker{
+        private String original;
+        private char[] input;
+        public StackCheck(String in){
+            this.original = in;
+            this.input = PallindromeChecker.preProcess(original).toCharArray();
+        }
+        @Override
+        public boolean check(char[] input) {
+            Stack<Character> st = new Stack<>();
+            for(char c : input){st.add(c);}
+            for(int i=0;i<input.length;i++){
+                if(st.pop() != input[i]){return false;}
+            }
+            return true;
+        }
+        public void printResult(){
+            PallindromeChecker.super.result(check(input), original, input);
+        }
+    }
+
+    static class DqueueCheck implements PallindromeChecker{
+        private String original;
+        private char[] input;
+        public DqueueCheck(String in){
+            this.original = in;
+            this.input = PallindromeChecker.preProcess(original).toCharArray();
+        }
+        @Override
+        public boolean check(char[] input){
+            int i=0,j=input.length-1;
+            Deque<Character> dq = new LinkedList<>();
+            for(char c : input){dq.add(c);}
+            while(dq.size()>1){
+                if(!dq.removeFirst().equals(dq.removeLast())){return false;}
+            }
+            return true;
+        }
+        public void printResult(){
+            PallindromeChecker.super.result(check(input), original, input);
         }
     }
     public static void main(String[] args){
@@ -33,10 +70,29 @@ public class U1{
         }
         System.out.println("System initialized successfully");
         String s = "Madam";
-
-        PallindromeChecker service = new PallindromeChecker(s);
-        service.result();
-    }
-    
+        PallindromeChecker service;
+        char choise; choise = new Scanner(System.in).next().toLowerCase().charAt(0);
+        switch(choise){
+            case 's':
+                service = new StackCheck(s);
+                break;
+            case 'q':
+                service = new DqueueCheck(s);
+                break;
+            default:
+                service = new PallindromeChecker() {
+                    String original = s;
+                    char[] input = PallindromeChecker.preProcess(original).toCharArray();
+                    @Override
+                    public boolean check(char[] input) {
+                        return PallindromeChecker.super.check(input);
+                    }
+                    public void printResult(){
+                        PallindromeChecker.super.result(check(input), original, input);
+                    }   
+                };
+                break;
+        }
+        service.printResult();
+    }  
 }
-    
